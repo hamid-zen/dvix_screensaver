b5d2e6a06590339b97134ce495b0b719f8cb3ef8
Saumoneta <saumoneta@ThinkPad>
2025-01-08 00:14:32 +0100

    finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,38 @@
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
 const unsigned int WIND_WIDTH = 800;
 const unsigned int WIND_HEIGHT = 600;
+const unsigned int FRAME_RATE = 60;
 
-sf::Vector2f updatePosition(sf::Vector2f const &current_position, sf::Vector2u const &object_size, float &speed) {
-    sf::Vector2f new_position = current_position;
+void updateSprite(sf::Sprite &dvix_sprite, sf::Vector2f &speed)
+{
+    sf::Vector2f new_position = dvix_sprite.getPosition();
 
-    // check if we arrived at the bound horizontally
-    if (new_position.x <= 0 || new_position.x + object_size.x >= WIND_WIDTH) {
-        speed *= -1;
+    // check if we arrived at the bound
+    if (new_position.x <= 0 || new_position.x + dvix_sprite.getGlobalBounds().width >= WIND_WIDTH)
+    {
+        dvix_sprite.setColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
+        speed.x *= -1;
     }
-    new_position.x += speed;
-    return new_position;
+    if (new_position.y <= 0 || new_position.y + dvix_sprite.getGlobalBounds().height >= WIND_HEIGHT)
+    {
+        dvix_sprite.setColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
+        speed.y *= -1;
+    }
+
+    new_position.x += speed.x;
+    new_position.y += speed.y;
+
+    dvix_sprite.setPosition(new_position);
 }
 
-int main() {
+int main()
+{
+
+    std::srand(std::time(nullptr));
 
     // Create a window
     sf::RenderWindow window(sf::VideoMode(WIND_WIDTH, WIND_HEIGHT), "SFML Window");
@@ -28,13 +46,15 @@ int main() {
     sf::Sprite dvix_sprite;
     dvix_sprite.setColor(sf::Color::White);
     dvix_sprite.setTexture(dvix_texture);
-    dvix_sprite.setPosition(WIND_WIDTH/2, WIND_HEIGHT/2);
+    dvix_sprite.setPosition(WIND_WIDTH / 2, WIND_HEIGHT / 2);
 
-    float speed = 5;
+    sf::Vector2f speed(FRAME_RATE / 15, FRAME_RATE / 15);
 
-    while (window.isOpen()) {
+    while (window.isOpen())
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event))
+        {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
@@ -42,8 +62,7 @@ int main() {
         window.clear(sf::Color::Black);
 
         // compute and set new position
-        sf::Vector2f new_position = updatePosition(dvix_sprite.getPosition(), dvix_texture.getSize(), speed);
-        dvix_sprite.setPosition(new_position);
+        updateSprite(dvix_sprite, speed);
 
         window.draw(dvix_sprite);
         window.display();
